a908d80ede8f56c9847d73b6bf8ebcaaae9f71f4
Suyeol Yun <syyun@mit.edu>
2022-12-24 00:37:17 -0500

    fix gym changes

---
+++ main.py
@@ -0,0 +1,229 @@
+import gym
+import numpy as np
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+import pickle
+import time
+import sys
+import math
+import seaborn as sns
+import pathlib
+
+from gym import logger as gymlogger
+# from gym.wrappers import Monitor
+gymlogger.set_level(40) #error only
+import tensorflow as tf
+import random
+import matplotlib
+# %matplotlib inline
+import math
+import glob
+import io
+import base64
+from IPython.display import HTML
+from IPython import display as ipythondisplay
+
+
+"""
+Utility functions to enable video recording of gym environment and displaying it
+To enable video, just do "env = wrap_env(env)""
+"""
+
+def show_video(save_path):
+  mp4list = glob.glob('%s/*.mp4' %save_path)
+  if len(mp4list) > 0:
+    mp4 = mp4list[0]
+    video = io.open(mp4, 'r+b').read()
+    encoded = base64.b64encode(video)
+    ipythondisplay.display(HTML(data='''<video alt="test" autoplay 
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+  else: 
+    print("Could not find video")
+    
+
+def wrap_env(env, save_path):
+  env = Monitor(env, save_path, force=True)
+  return env
+
+nbins = 10
+GAMMA = 0.9
+ALPHA = 0.01
+
+def max_dict(d): 
+    """
+    looking for the action that gives the maximum value for a given state
+    """
+    max_v = float('-inf')
+    for key, val in d.items():
+        if val > max_v:
+            max_v = val
+            max_key = key
+    return max_key, max_v
+
+def create_bins(): 
+    """
+    create bins to discretize the continuous observable state space
+    """
+    # obs[0] -> cart position --- -4.8 - 4.8
+    # obs[1] -> cart velocity --- -inf - inf
+    # obs[2] -> pole angle    --- -41.8 - 41.8
+    # obs[3] -> pole velocity --- -inf - inf
+
+    bins = np.zeros((4,nbins))
+    bins[0] = np.linspace(-4.8, 4.8, nbins)
+    bins[1] = np.linspace(-5, 5, nbins)
+    bins[2] = np.linspace(-.418, .418, nbins)
+    bins[3] = np.linspace(-5, 5, nbins)
+    return bins
+
+def assign_bins(observation, bins): 
+    """
+    discretizing the continuous observation space into state
+    """
+    state = np.zeros(4)
+    for i in range(4):
+        state[i] = np.digitize(observation[0][i], bins[i])
+    return state
+
+def get_state_as_string(state):
+    """
+    encoding the state into string as dictionary
+    """
+    string_state=''
+    for e in state:
+            string_state = string_state+str(int(e)).zfill(2)
+    return string_state
+
+def get_all_states_as_string():
+    states = []
+    for i in range (nbins+1):
+        for j in range (nbins+1):
+            for k in range(nbins+1):
+                for l in range(nbins+1):
+                    a=str(i).zfill(2)+str(j).zfill(2)+str(k).zfill(2)+str(l).zfill(2)
+                    states.append(a)
+    return states
+
+def initialize_Q():
+    """
+    initialize your Q table
+    """
+    Q = {}
+
+    all_states = get_all_states_as_string()
+    for state in all_states:
+        Q[state] = {}
+        for action in range(env.action_space.n):
+            Q[state][action] = 0
+    return Q #q is state-action pair
+
+def play_one_game(bins, Q, eps=0.5):
+    """
+    train 1 episode
+    """
+    observation = env.reset()
+    done = False
+    cnt = 0 # number of moves in an episode
+    state = get_state_as_string(assign_bins(observation, bins))
+    total_reward = 0
+
+    while not done:
+        cnt += 1
+        # np.random.randn() seems to yield a random action 50% of the time ?
+        if np.random.uniform() < eps:
+            act = env.action_space.sample() # epsilon greedy
+        else:
+            act = max_dict(Q[state])[0]
+        test = env.step(act)
+        observation, reward, done, _, _ = env.step(act)
+
+        total_reward += reward
+
+        if done and cnt < 200:
+            reward = -300
+
+        state_new = get_state_as_string(assign_bins(observation, bins))
+
+        a1, max_q_s1a1 = max_dict(Q[state_new])
+        Q[state][act] += ALPHA*(reward + GAMMA*max_q_s1a1 - Q[state][act])
+        state, act = state_new, a1
+
+    return total_reward, cnt
+
+def play_many_games(bins, N=10000):
+    """
+    train many episodes
+    """
+    Q = initialize_Q()
+
+    length = []
+    reward = []
+    for n in range(N):
+        #eps=0.5/(1+n*10e-3)
+        eps = 1.0 / np.sqrt(n+1)
+
+        episode_reward, episode_length= play_one_game(bins, Q, eps)
+
+        if n % 100 == 0:
+            clear_output(wait=True)
+            print("Episode: %d, Epislon: %.4f, Reward %d"%(n,eps,episode_reward))
+        length.append(episode_length)
+        reward.append(episode_reward)
+    env.close()
+    return length, reward, Q
+
+def plot_running_avg(totalrewards,title='Running Average',save=False,name='result'):
+    """
+    plotting the average reward during training
+    """
+    fig=plt.figure()
+    N = len(totalrewards)
+    running_avg = np.empty(N)
+    for t in range(N):
+        running_avg[t] = np.mean(totalrewards[max(0, t-100):(t+1)])
+    plt.plot(running_avg)
+    plt.title(title)
+    plt.xlabel("Iterations")
+    plt.ylabel("Reward")
+    plt.grid()
+    if save:
+        plt.savefig(name+'.png',bbox_inches='tight')
+    else:
+        plt.show()
+
+def play_policy(bins,Q,N=1000,render=False,delay=0.01):
+    """
+    run an environment using a trained policy
+    """
+    
+    totalReward=[]
+    steps=[]
+    for n in range(N):
+        observation=env.reset()
+        done=False
+        episodeReward=0
+        while not done:
+            if render:
+                env.render()
+                time.sleep(delay)
+            state=get_state_as_string(assign_bins(observation, bins))
+            act=max_dict(Q[state])[0]
+#             print(act)
+            observation,reward,done,_=env.step(act)
+            episodeReward+=reward
+        totalReward.append(episodeReward)
+    env.close()
+    return totalReward
+
+bins = create_bins()
+
+env = gym.make('CartPole-v0')
+episode_lengths, episode_rewards, expert_Q=play_many_games(bins,N=20000)
+
+plot_running_avg(episode_rewards)
+
+if __name__ == "__main__":
+    pass
